97b3950416697bae4c0d66e995051b60ec919604
josael-droid <josael.zurita@utp.ac.pa>
2025-09-08 11:16:04 -0500

    Subiendo Programas de ejemplo de los videos del 1 al 10 del curso de fundamentos de C# en Platzi

---
+++ Ejemplo1ds4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/// calculo del area de un rectangulo
+
+int ladoA;
+int ladoB;
+int area;
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToInt32(Console.ReadLine());    
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToInt32(Console.ReadLine());
+
+area = ladoA * ladoB;
+
+Console.WriteLine("El area del rectangulo es: " + area);
+
---
+++ Ejemplo2ds4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
+var radio = 1.1;
+var resultado = 1.1;
+
+const double PI = 3.1416;   
+Console.WriteLine("Bienvenidos, calculemos circulos!");
+
+Console.WriteLine("Ingrese el radio del circulo:");
+radio = Convert.ToDouble(Console.ReadLine());
+  
+resultado = PI * radio * radio;
+
+Console.WriteLine($"El area del circulo es: {resultado}");
