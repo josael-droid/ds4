54a3a0d4ef73efc49941d3017565f25d8bf08c45
josael-droid <josael.zurita@utp.ac.pa>
2025-09-10 10:30:12 -0500

    Subiendo ejemplos de los videos del 11 al 13 del curso de Fundamentos de C#

---
+++ Ejemplo3ds4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+bool valor1 = true;
+bool valor2 = true;
+bool valor3 = true;
+
+bool resultAnd = valor1 && valor2 && valor3;
+Console.WriteLine(resultAnd);
+
+bool resultOr = valor1 || valor2 || valor3;
+Console.WriteLine(resultOr);
+
+bool resultAndOr = (valor1 && valor2) || valor3;
+Console.WriteLine(resultAndOr);
+
+bool resultadoNot = !valor1;
+Console.WriteLine(resultadoNot);
+
+bool resultadoXor = valor1 ^ valor2; //XOR = not or (es decir lo opuesto a la or)
+Console.WriteLine(resultadoXor);
+
+resultadoXor = (valor1 ^ valor2) ^ valor3; //XOR ^ XOR es decir primero saca el binario T/F de la primera Xor y luego lo compara con la segunda
+Console.WriteLine(resultadoXor);
